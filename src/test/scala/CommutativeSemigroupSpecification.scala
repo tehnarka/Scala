5850cf7c666bd9d4e1226f67ee9afaed7491ed0e
tehnarka <primat.kpi@gmail.com>
2021-05-31 12:14:36 +0300

    edits

--- src/test/scala/CommutativeSemigroupSpecification.scala
+++ src/test/scala/CommutativeSemigroupSpecification.scala
@@ -1,18 +1,17 @@
 import org.scalacheck.Prop.forAll
 import org.scalacheck.{Arbitrary, Gen, Prop}
 import org.scalacheck.Properties
-import scala.concurrent.duration._
 import CommutativeSemigroupOps._
   
 object CommutativeSemigroupSpecification extends Properties("CommutativeSemigroup") {
   property("assoc") = forAll
   {
     (x: Duration, y: Duration, z: Duration) =>
-        (x/3 ++ y/3) ++ z/3 == x/3 ++ (y/3 ++ z/3)
+        (x ++ y) ++ z == x ++ (y ++ z)
   }
   property("commut") = forAll
   {
     (x: Duration, y: Duration) =>
-      x/2 ++ y/2 == y/2 ++ x/2
+      x ++ y == y ++ x
   }
 }
